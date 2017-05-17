074fc43d97c17c8ef990573356cd79969288bd7b
Jaked222 <Asphynx222@gmail.com>
2017-05-17 19:41:56 -0400

    remove submodules

---
+++ reduxProject2/src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+	return [
+		{ title: 'js is cool', pages: 200 },
+		{ title: 'js is not cool', pages:30 },
+		{ title: 'got lol', pages: 500},
+		{ title: 'lotr', pages: 1000}
+	]
+}
