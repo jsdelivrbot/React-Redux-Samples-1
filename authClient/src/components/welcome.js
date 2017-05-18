616e7b0962f17d35b51c5762213de396a0d89061
Jaked222 <Asphynx222@gmail.com>
2017-05-17 21:24:20 -0400

    store

---
+++ authClient/src/components/welcome.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import Header from './header';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+				Welcome to this auth app!
+      </div>
+    );
+  }
+}
--- authClient/src/index.js
+++ authClient/src/index.js
@@ -11,14 +11,26 @@ import Signout from './components/auth/signout';
 import Signup from './components/auth/signup';
 import Feature from './components/feature';
 import RequireAuth from './components/auth/require_auth';
+import Welcome from './components/welcome';
 import reducers from './reducers';
+import { AUTH_USER } from './actions/types';
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+const store = createStoreWithMiddleware(reducers);
+
+const token = localStorage.getItem('token');
+
+// if we have a token, consider the user signed in
+if (token) {
+  // update application state before it's rendered
+  store.dispatch({ type: AUTH_USER });
+}
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
     <Router history={browserHistory}>
       <Route path="/" component={App} >
+        <IndexRoute component={Welcome}/>
         <Route path="signin" component={Signin} />
         <Route path="signout" component={Signout} />
         <Route path="signup" component={Signup} />
