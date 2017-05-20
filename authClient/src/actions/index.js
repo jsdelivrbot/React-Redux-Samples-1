0954e93d7283e234b1a59c1816996559a9a0d7d9
Jaked222 <Asphynx222@gmail.com>
2017-05-20 10:59:18 -0400

    wrapup

--- authClient/src/actions/index.js
+++ authClient/src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { browserHistory } from 'react-router';
-import { AUTH_USER, AUTH_ERROR, UNAUTH_USER } from './types';
+import { AUTH_USER, AUTH_ERROR, UNAUTH_USER, FETCH_MESSAGE } from './types';
 const ROOT_URL = 'http://localhost:3090';
 
 export function signinUser({ email, password }) {
@@ -43,9 +43,24 @@ export function signoutUser() {
 
 	return { type: UNAUTH_USER };
 }
+
 export function authError(error) {
 	return { 
 		type: AUTH_ERROR,
 		payload: error
+	}
+}
+
+export function fetchMessage() {
+	return function(dispatch) {
+		axios.get(ROOT_URL, { 
+			headers: { authorization: localStorage.getItem('token')}
+		})
+		.then(response => {
+			dispatch({
+				type: FETCH_MESSAGE,
+				payload: response.data.message
+			});
+		});
 	}
 }
